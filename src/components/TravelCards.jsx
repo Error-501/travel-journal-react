5e4677116b073f0a6e16c29ad04b89abf038680a
DineshKumar Murugasamy <dineshkumar.murugasamy@striim.com>
2024-11-02 15:15:25 +0530

    Added separator line between cards

--- src/components/TravelCards.jsx
+++ src/components/TravelCards.jsx
@@ -1,24 +1,27 @@
 export default function TravelCards(props) {
     const country = props.item.country.toUpperCase();
     return (
-        <div className="travelCards">
-            <img className="travel--image" 
-                src={`/images/${props.item.image}`} alt={props.item.image}></img>
-            <section className="card--content">
-                <header className="card--header">
-                    <img className="travel--navigate" src="/images/navigate.svg" alt="navigate-icon"></img>
-                    <h6 className="card--country">{country}</h6>
-                    <span>
-                        <a className="maps--link" href={props.item.mapUrl}
-                            rel="noopener noreferrer" target="_blank">
-                            View on Google Maps
-                        </a>
-                    </span>    
-                </header>
-                <h1 className="card--place">{props.item.place}</h1>
-                <h4 className="card--date">{props.item.travelDates}</h4>
-                <p className="card--summary">{props.item.summary}</p>
-            </section>
+        <div className="card--cont">
+            <div className="travelCards">
+                <img className="travel--image" 
+                    src={`/images/${props.item.image}`} alt={props.item.image}></img>
+                <section className="card--content">
+                    <header className="card--header">
+                        <img className="travel--navigate" src="/images/navigate.svg" alt="navigate-icon"></img>
+                        <h6 className="card--country">{country}</h6>
+                        <span>
+                            <a className="maps--link" href={props.item.mapUrl}
+                                rel="noopener noreferrer" target="_blank">
+                                View on Google Maps
+                            </a>
+                        </span>    
+                    </header>
+                    <h1 className="card--place">{props.item.place}</h1>
+                    <h4 className="card--date">{props.item.travelDates}</h4>
+                    <p className="card--summary">{props.item.summary}</p>
+                </section>
+            </div>
+            <hr className="card--separator" />
         </div>
     )
 }
